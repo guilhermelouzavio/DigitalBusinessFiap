f331d5df16421ce069cefd54151be5efb8becd29
Guilherme Louzavio <guilherme.louzavio@gmail.com>
2020-09-02 11:51:33 -0300

    Update data.sql

--- Nac1DigitalBusiness/src/main/resources/data.sql
+++ Nac1DigitalBusiness/src/main/resources/data.sql
@@ -1,1 +1,1 @@
-INSERT INTO Paciente(codigo, nome, nr_cpf, tp_sanguineo, ds_genero, email,nr_telefone) VALUES (SQ_TAREFA.nextval, 'guilherme', 'guilherme', 'guilherme',  'guilherme', 'guilherme',6546546);
+INSERT INTO Paciente(codigo, nome, nr_cpf, tp_sanguineo, ds_genero, email,nr_telefone) VALUES (SQ_TAREFA.nextval, 'guilherme', '473.119.898-89', 'A+',  'M', 'guilherme.louzavio4@gmail.com',6546546);
